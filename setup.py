264c31948ff457b95994c9962102f06b49234fef
Timophey999 <timoxa.m.widows@gmail.com>
2024-12-10 17:48:40 +0700

    Initial Commit

--- setup.py
+++ setup.py
@@ -6,16 +6,11 @@ setup(
     description='Модуль для взаимодействия с XUI API, включая управление входящими соединениями и клиентами.',
     long_description=open('README.md', encoding='utf-8').read(),
     long_description_content_type='text/markdown',
-    author='Ваше Имя',
+    author='Timofey Mikheev',
     author_email='timoxa.m.is@icloud.com',
     url='https://github.com/Timophey999/connectXUI.git',
-    packages=find_packages(),
-    install_requires=[
-        'aiohttp',
-        'pydantic',
-        'pytest',
-        'aioresponses'
-    ],
+    packages=find_packages(exclude=("tests",)),
+    install_requires=open("requirements.txt").read().splitlines(),
     classifiers=[
         'Development Status :: 3 - Alpha',
         'Intended Audience :: Developers',
@@ -23,5 +18,5 @@ setup(
         'Programming Language :: Python :: 3',
         'Programming Language :: Python :: 3.10',
     ],
-    python_requires='>=3.10',
+    python_requires='>=3.7',
 )
